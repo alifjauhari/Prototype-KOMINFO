2ac7f845feb231d255593b14c40ecfcb6c862377
alifjauhari <alifkamal@student.telkomuniversity.ac.id>
2022-03-04 09:49:17 +0700

    Add 360 scene

---
+++ Assets/Script/360/Transition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    private bool transition;
+
+    public float maxFOV;
+    public float zoomRate;
+    public float currentFOV;
+
+    void Start()
+    {
+        currentFOV = GetComponent<Camera>().fieldOfView;
+    }
+
+    private void FixedUpdate()
+    {
+        if (transition)
+        {
+            if (currentFOV > maxFOV)
+            {
+                currentFOV -= zoomRate;
+                GetComponent<Camera>().fieldOfView = currentFOV;
+                Debug.Log("currentFOX = " + currentFOV);
+            }
+            else if (currentFOV == maxFOV)
+            {
+                StopCoroutine("ZoomTime");
+                transition = false;
+            }
+        }
+    }
+
+    IEnumerator ZoomTime()
+    {
+        yield return new WaitForSeconds(0.05f);
+        
+    }
+
+    public void ZoomInTransition()
+    {
+        StartCoroutine("ZoomTime");
+        transition = true;
+    }
+
+
+
+}
